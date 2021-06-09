96c12245d3e27128071034375be5a14726d6f64d
Kishan Nirghin <kishan@itslogical.nl>
2021-06-09 11:01:14 +0200

    feat: add screening questions

---
+++ . Screening Questions/socialMediaPlatform.js
@@ -0,0 +1,20 @@
+// Remove dupplicates in place
+//
+// input: [1, 2, 3, 3, 4, 4]
+// output: 4
+// 
+// What is the runtime complexity
+// What edge cases are there to take into account
+
+// Implement the Math.pow function
+// 
+// input 2,9
+// output 512
+//
+// What is the runtime complexity
+// can it be done in O(logn)?
+//  => yes 
+//
+// What is the spacetime complexity
+// can it be done in O(logn)?
+//  => yes
