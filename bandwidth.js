4153ee5ffde4dd437070e3bfb45e7eb8294d5d52
Kishan Nirghin <kishan@itslogical.nl>
2023-07-25 00:02:10 +0200

    chore: cleanup repo

---
+++ bandwidth.js
@@ -0,0 +1,9 @@
+/**
+ * VIDEO SHARING PLATFORM
+ *
+ * 5 million active users
+ * 500.000 videos uploaded daily
+ * average video size: 300MB
+ *
+ * 500000 * 300MB = 300GB * 500 = 1.5TB * 100 = 150TB / DAILY UPLOADS
+ */
