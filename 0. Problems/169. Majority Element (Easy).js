9bcda956fa02bd0eb70862fc735878c85edac96b
Kishan Nirghin <kishan@itslogical.nl>
2023-08-17 22:15:08 +0200

    feat: add leetcode problem 169 (proper)

--- 0. Problems/169. Majority Element (Easy).js
+++ 0. Problems/169. Majority Element (Easy).js
@@ -3,22 +3,44 @@
  * @return {number}
  */
 var majorityElement = function (nums) {
-  const count = {};
+  let winner;
+  let count = 0;
 
-  for (let num of nums) {
-    if (num in count) {
-      count[num]++;
+  for (let i = 0; i < nums.length; i++) {
+    if (count === 0) {
+      winner = nums[i];
+      count = 1;
+    } else if (winner === nums[i]) {
+      count++;
     } else {
-      count[num] = 1;
+      count--;
     }
   }
 
-  let goalNum = Math.floor(nums.length / 2);
-  for (const c in count) {
-    if (count[c] > goalNum) {
-      return c;
-    }
-  }
-
-  return -1;
+  return winner;
 };
+
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// var majorityElement = function (nums) {
+//   const count = {};
+
+//   for (let num of nums) {
+//     if (num in count) {
+//       count[num]++;
+//     } else {
+//       count[num] = 1;
+//     }
+//   }
+
+//   let goalNum = Math.floor(nums.length / 2);
+//   for (const c in count) {
+//     if (count[c] > goalNum) {
+//       return c;
+//     }
+//   }
+
+//   return -1;
+// };
