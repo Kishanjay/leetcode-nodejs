aa0bec0428cb658133c90d53b54b6f8d4c68b1a5
Kishan Nirghin <kishan@itslogical.nl>
2023-08-26 18:38:30 +0200

    feat: add proper runtime solution for lc 11

--- top250_speedrun/11. Container With Most Water.js
+++ top250_speedrun/11. Container With Most Water.js
@@ -3,6 +3,35 @@
  * @return {number}
  */
 var maxArea = function (height) {
+  let startI = 0;
+  let endI = height.length - 1;
+
+  let maxArea = 0;
+  while (startI !== endI) {
+    let width = endI - startI;
+
+    const bar1 = height[startI];
+    const bar2 = height[endI];
+
+    if (bar1 > bar2) {
+      maxArea = Math.max(maxArea, bar2 * width);
+
+      endI--;
+    } else {
+      maxArea = Math.max(maxArea, bar1 * width);
+
+      startI++;
+    }
+  }
+
+  return maxArea;
+};
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
   let maxHeight = 0;
 
   let i = 0;
