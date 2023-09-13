3abaab9313edff92aa8cab0c1dcecb130105f22c
Kishan Nirghin <kishan@itslogical.nl>
2023-09-13 11:44:45 +0200

    feat: add leetcode problem 168 #2

--- 0. Problems/168. Excel Sheet Column Title.js
+++ 0. Problems/168. Excel Sheet Column Title.js
@@ -2,7 +2,25 @@
  * @param {number} n
  * @return {string}
  */
+
+const charcodebase = "A".charCodeAt(0) - 1;
 var convertToTitle = function (n) {
+  let result = "";
+
+  while (n > 0) {
+    let ci = n % 26;
+    if (ci === 0) {
+      ci = 26;
+    }
+
+    result = String.fromCharCode(charcodebase + ci) + result;
+    n -= ci;
+    n = Math.floor(n / 26);
+  }
+  return result;
+};
+
+var convertToTitle2 = function (n) {
   if (n < 27) return String.fromCharCode(n + 64);
   let result = "";
   while (n > 0) {
