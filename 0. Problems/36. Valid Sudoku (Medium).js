d0f1d330776d53d62209b8f634c5b2904b1bd0f7
Kishan Nirghin <kishan@itslogical.nl>
2023-09-20 23:20:58 +0200

    feat: add leetcode problem 36

--- 0. Problems/36. Valid Sudoku (Medium).js
+++ 0. Problems/36. Valid Sudoku (Medium).js
@@ -45,3 +45,88 @@ var isValidSudoku = function (board) {
 
   return true;
 };
+
+var isValidSudoku = function (board) {
+  for (let i = 0; i < 9; i++) {
+    const row = {};
+    const col = {};
+    const grid = {};
+
+    for (let j = 0; j < 9; j++) {
+      if (board[i][j] in row && board[i][j] !== ".") {
+        return false;
+      } else {
+        row[board[i][j]] = true;
+      }
+
+      if (board[j][i] in col && board[j][i] !== ".") {
+        return false;
+      } else {
+        col[board[j][i]] = true;
+      }
+
+      // Start row + row offset because of j.
+      let rowI = ~~(i / 3) * 3 + ~~(j / 3);
+
+      // Start col + col offset because of j.
+      let colI = (i % 3) * 3 + (j % 3);
+
+      const gval = board[rowI][colI];
+      if (gval in grid && gval !== ".") {
+        return false;
+      } else {
+        grid[gval] = true;
+      }
+    }
+  }
+  return true;
+};
+
+var isValidSudoku = function (board) {
+  const rows = {};
+  const cols = {};
+  const grids = {};
+
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      const val = board[row][col];
+      if (val === ".") {
+        continue;
+      }
+
+      if (row in rows) {
+        if (val in rows[row]) {
+          return false;
+        }
+        rows[row][val] = true;
+      } else {
+        rows[row] = { [val]: true };
+      }
+
+      if (col in cols) {
+        if (val in cols[col]) {
+          return false;
+        }
+        cols[col][val] = true;
+      } else {
+        cols[col] = { [val]: true };
+      }
+
+      // get the grid index -> how?
+      const basedOnRow = ~~(row / 3) * 3;
+      const basedOnCol = ~~(col / 3);
+
+      const gridI = basedOnRow + basedOnCol;
+
+      if (gridI in grids) {
+        if (val in grids[gridI]) {
+          return false;
+        }
+        grids[gridI][val] = true;
+      } else {
+        grids[gridI] = { [val]: true };
+      }
+    }
+  }
+  return true;
+};
