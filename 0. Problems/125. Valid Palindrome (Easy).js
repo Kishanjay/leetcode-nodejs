d579e87477c514cc531dc6d06d55a5d73746edb9
Kishan Nirghin <kishan@itslogical.nl>
2023-09-23 16:18:23 +0200

    feat: add leetcode problem 125 (perfomance + short)

--- 0. Problems/125. Valid Palindrome (Easy).js
+++ 0. Problems/125. Valid Palindrome (Easy).js
@@ -24,3 +24,57 @@ var isPalindrome = function (s) {
   }
   return true;
 };
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+  const aVal = "a".charCodeAt(0);
+  const zVal = "z".charCodeAt(0);
+
+  const ZVal = "Z".charCodeAt(0);
+  const val0 = "0".charCodeAt(0);
+  const val9 = "9".charCodeAt(0);
+
+  const deltaCase = zVal - ZVal;
+
+  let i = 0;
+  let j = s.length - 1;
+  while (i < j) {
+      let iVal = s.charCodeAt(i);
+      if (iVal <= ZVal && !(iVal >= val0 && iVal <= val9)) {
+          iVal += deltaCase;
+      }
+
+      let jVal = s.charCodeAt(j);
+      if (jVal <= ZVal && !(jVal >= val0 && jVal <= val9)) {
+          jVal += deltaCase;
+      }
+
+      if((iVal < aVal || iVal > zVal) && !(iVal >= val0 && iVal <= val9)) {
+          i++;
+          continue;
+      }
+      if((jVal < aVal || jVal > zVal) && !(jVal >= val0 && jVal <= val9)) {
+          j--;
+          continue;
+      }
+
+      if (iVal !== jVal){
+          return false;
+      }
+
+      i++;
+      j--;
+  }
+
+  return true;
+};
+
+
+var isPalindrome = function(s) {
+  s = s.toLowerCase().replace(/[^0-9a-z]+/g,"")
+  const sRev = s.split("").reverse().join("")
+  return s === sRev
+};
